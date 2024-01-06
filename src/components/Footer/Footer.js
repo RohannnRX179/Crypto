a09fcdef451a7f26dcb80781b688e7632355f7b5
LOHANNNN1 <rohanrathod197614@gmail.com>
2024-01-06 20:44:24 +0530

    Add components directory

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <div class="footer">
+            <p>Made with ♥️ by <a href = "" className = "link">Rohan Rathod</a></p>
+            <p><a href=""><i className = "fa fa-github fa-2x link"></i></a></p>
+        </div>
+    )
+}
+
+export default Footer;
